149d6705bcc3d255c1d78b05733e9730602d0a32
ThatFloridaMan <white.nathan14@gmail.com>
2025-08-10 13:39:37 -0400

    Update SCR_CampaignBuildingGadgetToolLimitedComponent.c

--- scripts/game/FloridaMan/Building/SCR_CampaignBuildingGadgetToolLimitedComponent.c
+++ scripts/game/FloridaMan/Building/SCR_CampaignBuildingGadgetToolLimitedComponent.c
@@ -3,7 +3,7 @@
 //! First use of GadgetActivate keybind will start the preview, a second will place it
 modded class SCR_CampaignBuildingGadgetToolComponent : SCR_GadgetComponent
 {
-	[Attribute(defvalue: "-1", desc: "Maximum number of placement for this tool")]
+	[Attribute(desc: "Maximum number of placement for this tool")]
 	protected int m_iACE_Trenches_MaxPrefabCount;
 	
 	protected int m_iACE_Trenches_CurrentPrefabCount = 0;
@@ -11,7 +11,7 @@ modded class SCR_CampaignBuildingGadgetToolComponent : SCR_GadgetComponent
 	override void ACE_Trenches_RequestPlace() 
 	{
 		
-		if ((m_iACE_Trenches_MaxPrefabCount != -1) && (m_iACE_Trenches_CurrentPrefabCount >= m_iACE_Trenches_MaxPrefabCount))
+		if ((!m_iACE_Trenches_MaxPrefabCount) && (m_iACE_Trenches_CurrentPrefabCount >= m_iACE_Trenches_MaxPrefabCount))
 			return;
 		
 		super.ACE_Trenches_RequestPlace();
